1ba4bdeae9fa2f53ea9c7cf9bfb3ef5921c12f80
Perry Vargas <perry.vargas@asu.edu>
2015-07-08 15:23:46 -0700

    First stab at writing out the test_mission)data.py

---
+++ tests/test_mission_data.py
@@ -0,0 +1,20 @@
+
+import os
+from planetaryimage.pds3image import PDS3Image
+import json
+
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'mission_data/')
+with open(os.path.join(DATA_DIR, 'data.json'), 'r') as r:
+    data = json.load(r)
+
+
+def test_mission_data():
+    for file_name in data.keys():
+        image_path = os.path.join(DATA_DIR, file_name)
+        try:
+            image = PDS3Image.open(image_path)
+            assert data[file_name]['opens'] == "True"
+            assert data[file_name]['label'] == image.label.items()[0][1]
+        except:
+            assert data[file_name]['opens'] == "False"
