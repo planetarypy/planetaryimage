b6a00277efd6a69eced6a8e2d48e42e4acac8bcb
Perry Vargas <perry.vargas@asu.edu>
2015-07-13 17:17:35 -0700

    New test_mission_data

--- tests/test_mission_data.py
+++ tests/test_mission_data.py
@@ -2,30 +2,36 @@
 # -*- coding: utf-8 -*-
 import os
 from planetaryimage.pds3image import PDS3Image
-import json
-import pytest
+from planetary_test_data import PlanetaryTestDataProducts
 import pvl
 
 
-DATA_DIR = os.path.join(os.path.dirname(__file__), 'mission_data')
-
-
-@pytest.mark.skipif(not(os.path.exists(os.path.join(DATA_DIR, 'data.json'))),
-                    reason="data.json is not present, use get_mission_data")
 def test_mission_data():
-    with open(os.path.join(DATA_DIR, 'data.json'), 'r') as r:
-        data = json.load(r)
-    for file_name in data.keys():
-        image_path = os.path.join(DATA_DIR, file_name)
+    data_products = PlanetaryTestDataProducts()
+
+    for file_name in data_products.products:
+        image_path = os.path.join(data_products.directory, file_name)
         try:
             image = PDS3Image.open(image_path)
-            assert data[file_name]['opens'] == "True"
-            assert data[file_name]['label'] == image.label.items()[0][1]
+            assert data_products.mission_data[file_name]['opens'] == "True"
+            data_label = data_products.mission_data[file_name]['label']
+            for key in data_label:
+                try:
+                    if isinstance(data_label[key], dict):
+                        sub_dict = data_label[key]
+                        for sub_key in sub_dict:
+                            assert sub_dict[sub_key] == \
+                                image.label[key][sub_key]
+                    else:
+                        assert data_label[key] == image.label[key]
+                except:
+                    print ("Problem with %s label" % (file_name))
         except (pvl.decoder.ParseError, KeyError, UnicodeDecodeError,
                 ValueError):
             try:
-                assert data[file_name]['opens'] == "False"
+                assert data_products.mission_data[file_name]['opens'] \
+                    == "False"
             except:
-                print (file_name, "is marked as True and should be false")
+                print ("%s is marked as True & should be false" % (file_name))
         except AssertionError:
-            print (file_name, "is marked as False and should be True")
+            print ("%s is marked as False & should be True" % (file_name))
